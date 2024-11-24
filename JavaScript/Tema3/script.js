811d4a533085a26222855f0d7e1e29ecaf448121
Melk236 <elkhassalmohamed05@gmail.com>
2024-11-24 21:26:15 +0100

    javascript

--- JavaScript/Tema3/script.js
+++ JavaScript/Tema3/script.js
@@ -1,11 +1,35 @@
 function matricula() {
-    const numeros = [];
+    var n;
 
+    do {
+        n = prompt('Introduzca una matricula');
+        if (n.length != 4 || isNaN(n) || n.indexOf(' ') >= 0) {
+            alert('Error, introduzca un numero con cuatro cifras');
+        }
+    } while (n.length != 4 || isNaN(n) || n.indexOf(' ') >= 0);
 
-    n = prompt('Introduzca una matricula');
-
+    var numeros = n.split('', n);
+    numeros = numeros.map(Number);
+    numeros.sort();
     
-    
 
+    if (numeros[0] == numeros[1] && numeros[0] == numeros[2] && numeros[0]==numeros[3]) {
+        document.write('La matricula: '+n+' es un poker');
+    }
+    else if(numeros[0] == numeros[1]-1 && numeros[1] == numeros[2]-1 && numeros[2]==numeros[3]-1){
+
+        document.write('La matricula: '+n+' es una escalera completa');
+
+    }
+    else if((numeros[0] == numeros[1]-1 && numeros[1] == numeros[2]-1)||(numeros[1]==numeros[2]-1&&numeros[2]==numeros[3]-1) ){
+        document.write('La matricula: '+n+' es una escalera simple');
+    }
+    else if((numeros[0] == numeros[1] && numeros[1] == numeros[2])||(numeros[1]==numeros[2]&&numeros[2]==numeros[3])){
+        document.write('La matricula: '+n+' es un trio');
+    }
+    else if(numeros[0]==numeros[1]||numeros[2]==numeros[3]){
+        document.write('La matricula: '+n+' es una doble pareja');
+    }
 
 }
+matricula();
