faeac81aaa20987cc6c6d668b639c6f7ecfd7c93
Melk236 <elkhassalmohamed05@gmail.com>
2024-11-25 12:21:40 +0100

    w

--- JavaScript/Tema3/script.js
+++ JavaScript/Tema3/script.js
@@ -1,5 +1,5 @@
 function matricula() {
-    var n;
+    var n,doblePareja=0,trio=0,rscaleraSimple=0,escaleraCompleta=0,pocker=0;
 
     do {
         n = prompt('Introduzca una matricula');
@@ -11,24 +11,49 @@ function matricula() {
     var numeros = n.split('', n);
     numeros = numeros.map(Number);
     numeros.sort();
+
+
+    if (numeros[0] == numeros[1] && numeros[0] == numeros[2] && numeros[0] == numeros[3]) {
+        document.write('La matricula: ' + n + ' es un poker');
+    }
+    else if (numeros[0] == numeros[1] - 1 && numeros[1] == numeros[2] - 1 && numeros[2] == numeros[3] - 1) {
+
+        document.write('La matricula: ' + n + ' es una escalera completa');
+
+    }
+    else if ((numeros[0] == numeros[1] - 1 && numeros[1] == numeros[2] - 1) || (numeros[1] == numeros[2] - 1 && numeros[2] == numeros[3] - 1)) {
+        document.write('La matricula: ' + n + ' es una escalera simple');
+    }
+    else if ((numeros[0] == numeros[1] && numeros[1] == numeros[2]) || (numeros[1] == numeros[2] && numeros[2] == numeros[3])) {
+        document.write('La matricula: ' + n + ' es un trio');
+    }
+    else if (numeros[0] == numeros[1] && numeros[2] == numeros[3]) {
+
+        document.write('La matricula: ' + n + ' es una doble pareja');
+
+    }
+   
+    
     
 
-    if (numeros[0] == numeros[1] && numeros[0] == numeros[2] && numeros[0]==numeros[3]) {
-        document.write('La matricula: '+n+' es un poker');
-    }
-    else if(numeros[0] == numeros[1]-1 && numeros[1] == numeros[2]-1 && numeros[2]==numeros[3]-1){
+}
 
-        document.write('La matricula: '+n+' es una escalera completa');
+function probabilidad(){
 
-    }
-    else if((numeros[0] == numeros[1]-1 && numeros[1] == numeros[2]-1)||(numeros[1]==numeros[2]-1&&numeros[2]==numeros[3]-1) ){
-        document.write('La matricula: '+n+' es una escalera simple');
-    }
-    else if((numeros[0] == numeros[1] && numeros[1] == numeros[2])||(numeros[1]==numeros[2]&&numeros[2]==numeros[3])){
-        document.write('La matricula: '+n+' es un trio');
-    }
-    else if(numeros[0]==numeros[1]||numeros[2]==numeros[3]){
-        document.write('La matricula: '+n+' es una doble pareja');
+    for(let i=0;i<10000;i++){
+        if(i<10){
+            if(i=0){
+                pocker++;
+            }
+        }
+        else if(i<100){
+            i=String(i);
+
+            i='00'+i;
+           
+
+        }
+
     }
 
 }
