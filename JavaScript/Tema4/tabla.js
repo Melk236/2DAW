a4db40a3052a194afe1b6c4211947c1857e48042
Melk236 <elkhassalmohamed05@gmail.com>
2025-01-07 17:36:10 +0100

    a

--- JavaScript/Tema4/tabla.js
+++ JavaScript/Tema4/tabla.js
@@ -1,6 +1,8 @@
-function grosorCelda(){
+function grosorCelda() {
 
-    let grosor=document.getElementById('tabla').offsetWidth;
-    document.getElementById('parrafo').value=grosor;
+    let tabla = document.getElementById('tabla1');//Guardamos la etiqueta table en la variable tabla
 
+    let estilo = window.getComputedStyle(tabla);//El método getComputedStyle nos permite acceder a los estilos finales del elemento
+
+    document.getElementById('parrafo').innerHTML = estilo.borderWidth;//Mostramos el borde de la tabla en una etiqueta p.
 }
