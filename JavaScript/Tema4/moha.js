d79cf01392788ac72b116036435e5b70aecd78fe
Melk236 <elkhassalmohamed05@gmail.com>
2025-01-18 12:27:09 +0100

    q

--- JavaScript/Tema4/moha.js
+++ JavaScript/Tema4/moha.js
@@ -9,7 +9,9 @@ window.onload = () => {
     let color6 = document.getElementsByClassName('color6')[0];
     let colorDefinitivo;
     let estilo;
+    colorDefinitivo = document.getElementsByClassName('seleccionado')[0];
 
+        estilo = window.getComputedStyle(colorDefinitivo);
     let tabla = document.createElement('table');
     let fila;
     let columnas
@@ -34,6 +36,7 @@ window.onload = () => {
 
     let contenedor = document.getElementById('zonadibujo');
     let pincel = document.getElementById('pincel');
+    pincel.innerHTML='Pincel desactivado';
     contenedor.appendChild(tabla);
     color1.addEventListener('click', () => {
         pincel.innerHTML = 'Pincel: Color Amarillo';
@@ -50,7 +53,7 @@ window.onload = () => {
     });
 
     color2.addEventListener('click', () => {
-        pincel.innerHTML = 'Pincel: Color Verde';
+      
         color2.classList.toggle('seleccionado');
         color1.classList.remove('seleccionado');
         color3.classList.remove('seleccionado');
@@ -64,7 +67,7 @@ window.onload = () => {
     });
 
     color3.addEventListener('click', () => {
-        pincel.innerHTML = 'Pincel: Color Negro';
+       
         color3.classList.toggle('seleccionado');
         color1.classList.remove('seleccionado');
         color2.classList.remove('seleccionado');
@@ -77,7 +80,7 @@ window.onload = () => {
 
     });
     color4.addEventListener('click', () => {
-        pincel.innerHTML = 'Pincel: Color Rojo';
+      
         color4.classList.toggle('seleccionado');
         color1.classList.remove('seleccionado');
         color3.classList.remove('seleccionado');
@@ -90,7 +93,7 @@ window.onload = () => {
 
     });
     color5.addEventListener('click', () => {
-        pincel.innerHTML = 'Pincel: Color Azul';
+       
         color5.classList.toggle('seleccionado');
         color1.classList.remove('seleccionado');
         color3.classList.remove('seleccionado');
@@ -103,7 +106,7 @@ window.onload = () => {
 
     });
     color6.addEventListener('click', () => {
-        pincel.innerHTML = 'Pincel: Color Blanco';
+        
         color6.classList.toggle('seleccionado');
         color1.classList.remove('seleccionado');
         color3.classList.remove('seleccionado');
@@ -130,6 +133,7 @@ window.onload = () => {
             activo=false;
         }
         else {
+            pincel.innerHTML='Pincel desactivado';
             for (let i = 0; i < columnas.length; i++) {
                 columnas[i].removeEventListener('mouseover', cambiarColor);
             }
@@ -139,6 +143,7 @@ window.onload = () => {
 
     });
     function cambiarColor(event){
+        pincel.innerHTML='Pincel activado';
         event.target.style.backgroundColor = estilo.backgroundColor;
     }
 
