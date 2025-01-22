b06d6b9aee78e05c9c9558eccd0918238868da3d
Melk236 <elkhassalmohamed05@gmail.com>
2025-01-22 10:21:18 +0100

    e

--- JavaScript/Tema4/tabla.js
+++
@@ -1,8 +0,0 @@
-function grosorCelda() {
-
-    let tabla = document.getElementById('tabla1'); //Guardamos la etiqueta table en la variable tabla
-  
-    let estilo=tabla.getAttribute('border');//Guardamos en la variable estilo el atributo borde.
-    document.getElementById('parrafo').innerHTML ='El borde de la tabla es: '+ estilo; //Mostramos el borde de la tabla en una etiqueta p.
-
-}
