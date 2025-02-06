c1616d3a13587a613b97f0f0f3e93bfac15b70fc
Melk236 <elkhassalmohamed05@gmail.com>
2025-02-06 14:40:58 +0100

    w

--- JavaScript/Tema4/examen.js
+++ JavaScript/Tema4/examen.js
@@ -1,5 +1,25 @@
-function abrir(){
-    if(document.getElementById('nombre')=='alumno'){
-        
-    }    
-}
+let boton=document.getElementById('boton');
+    boton.addEventListener('click',()=>{
+        let usuario=document.getElementById('nombre').value;
+    
+        if(usuario!=='usuario' && usuario!=='administrador'){
+         document.getElementById('error').innerHTML='Usuario o contraseña incorrectos';
+        }
+        else{
+         window.location.href='formulario.html';
+        }
+        function almacenar(){
+            if(usuario=='usuario'){
+            let contenedor=document.getElementById('tabla');
+            let tabla=document.createElement('table');
+            tabla.appendChild(contendor);
+            let columna=d
+
+
+            }
+        }
+
+
+    });
+    
+
