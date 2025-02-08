bb6833c02888dec710f53652574d949d59e6d2a4
Melk236 <elkhassalmohamed05@gmail.com>
2025-02-08 20:54:06 +0100

    e

--- JavaScript/Tema4/examen.js
+++ JavaScript/Tema4/examen.js
@@ -1,45 +1,57 @@
-let boton=document.getElementById('boton');
+let boton = document.getElementById('boton');
 let tabla;
-    
-        boton.addEventListener('click',()=>{
-
-            if(document.getElementById('nombre').value=='usuario' || document.getElementById('administrador').value=='administrador'){
-                window.location.href='formulario.html';
-            }
-           
 
 
+function enviar() {
+   let nombre=document.getElementById('nombre').value;
+    if (document.getElementById('nombre').value == 'usuario' || document.getElementById('nombre').value == 'administrador') {
+        
+        alert(nombre);
+        localStorage.setItem('nombre',nombre);
+        window.location.href = 'formulario.html';
+      
+     
+    }
+    else {
+        document.getElementById('error').innerHTML = 'Usuario incorrecto';
+    }
 
-        });
-        function almacenar(){
-            let contenedor=document.getElementById('tabla');
-            tabla=document.createElement('table');
-           contenedor.appendChild(tabla);
-           tabla.style.border='solid 1px';
-            let fila=document.createElement('tr');
-            tabla.appendChild(fila);
-            agregarCelda(fila,'Nombre');
-            agregarCelda(fila,'Descripción');
-            agregarCelda(fila,'Importancia');
-            agregarCelda(fila,'Duración estimada');
-
-            
-           
-            
-        }
-
-        function agregarCelda(fila,contenido){
-            
-            let columna=document.createElement('td');
-            columna.style.border='solid 1px';
-             contenido=document.createTextNode(contenido);
-            columna.appendChild(contenido);
-            fila.appendChild(columna);
-
-        }
-       
+}
 
 
 
-    
+function almacenar() {
+    let nombre=localStorage.getItem('nombre');
+    alert(nombre);
+    if(nombre=='usuario'){
+    let contenedor = document.getElementById('tabla');
+    tabla = document.createElement('table');
+    contenedor.appendChild(tabla);
+    tabla.style.border = 'solid 1px';
+    let fila = document.createElement('tr');
+    tabla.appendChild(fila);
+    agregarCelda(fila, 'Nombre');
+    agregarCelda(fila, 'Descripción');
+    agregarCelda(fila, 'Importancia');
+    agregarCelda(fila, 'Duración estimada');
+    }
+
+
+
+}
+
+function agregarCelda(fila, contenido) {
+
+    let columna = document.createElement('td');
+    columna.style.border = 'solid 1px';
+    contenido = document.createTextNode(contenido);
+    columna.appendChild(contenido);
+    fila.appendChild(columna);
+
+}
+
+
+
+
+
 
