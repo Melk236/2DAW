aa256690a1b05da7382fec401151236268dec55c
Melk236 <elkhassalmohamed05@gmail.com>
2025-02-24 19:02:05 +0100

    a

--- JavaScript/Tema5/examen.js
+++ JavaScript/Tema5/examen.js
@@ -1,92 +1,142 @@
-
-window.onload = () => {
-    document.body.innerHTML = '<div id="cont"></div> <h2>Formulario acceso</h2><form><label>Usuario: </label><input type="text" id="usuario"><br><br><label>Contraseña: </label><input type="password" id="contraseña"><br><br><button type="button" onclick="validar()">Enviar</button></form> <p id="error"></p>'
-};
-
-let usuario, valid = false,tabla;
+let usuario = document.getElementById('usuario');
+let p = document.createElement('p');
 function validar() {
-    usuario = document.getElementById('usuario');
-    if (usuario.value == 'empleado' || usuario.value == 'administrador') {
-        document.body.innerHTML = '';
-        document.body.innerHTML = '<h2>Formulario Tabla</h2><form id="form"><label>Tarea: </label><input type="text" id="texto"><br><br><label>Descripción: </label><input type="text" id="descrip"><br><br><label>Importancia: </label><select id="importancia"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option></select><br><br><label>Duración estimada: </label><input type="number" id="duracion" min="1" max="120"><br><br> <button type="button" onclick="almacenar()">Almacenar</button></form><br><br>';
-    }
 
+    if (usuario.value == 'empleado' || usuario.value == 'administrador') {
+        localStorage.setItem('usuario', usuario.value);
+
+
+
+        window.location.href = 'form2.html';
+
+    }
     else {
 
-        let error = document.getElementById('error');
-        error.innerHTML = 'Error usuario o contraseña incorrectos';
+        p.innerHTML = '';
+        let contenido = document.createTextNode('Error, usuario o contraseña incorrectos');
+        p.appendChild(contenido);
+        document.body.appendChild(p);
     }
 }
+let tabla = document.createElement('table');
+
+let fila, celda, contenido, formulario, valid = false,boton;
 function almacenar() {
-    let form = document.getElementById('form');
-    
-    let fila, celda, contenido;
+    tabla.style.border = '1px solid black';
+    document.body.appendChild(tabla);
 
-    
-    if (usuario.value == 'empleado') {
+    usuario = localStorage.getItem('usuario');
+
+    if (usuario == 'empleado') {
         if (valid == false) {
-            tabla = document.createElement('table');
-             document.body.appendChild(tabla);
-             tabla.style.border = '1px solid';
-            fila = document.createElement('tr');
-            tabla.appendChild(fila);
-            celda = document.createElement('th');
-            celda.style.border = '1px solid';
-            contenido = document.createTextNode('Tarea');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-            celda = document.createElement('th');
-            celda.style.border = '1px solid';
-            contenido = document.createTextNode('Descripción');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-            celda = document.createElement('th');
-            celda.style.border = '1px solid';
-            contenido = document.createTextNode('Importancia');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-            celda = document.createElement('th');
-            celda.style.border = '1px solid';
-            contenido = document.createTextNode('Duración');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-            celda = document.createElement('th');
-            celda.style.border = '1px solid';
-            contenido = document.createTextNode('Realizado');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-            fila = document.createElement('tr');
-            tabla.appendChild(fila);
 
+
+            fila = document.createElement('tr');
+
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Tarea')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Descripción')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Importancia')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Duración')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            form = document.getElementById('form');
+            fila = document.createElement('tr');
             for (let i = 0; i < form.elements.length; i++) {
-                if (form.elements[i].type !== 'button') {
-                    celda = document.createElement('td');
-                    celda.style.border = '1px solid';
-                    contenido = document.createTextNode(form.elements[i].value);
-                    celda.appendChild(contenido);
-                    fila.appendChild(celda);
+
+                if (form.elements[i].type != 'button') {
+
+                    contenido = form.elements[i].value;
+                    agregarCelda(fila, contenido);
 
                 }
             }
-            celda = document.createElement('td');
-            celda.style.border = '1px solid';
-            contenido = document.createElement('input');
-            contenido.setAttribute('type', 'checkbox');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-            valid=true;
+            valid = true;
         }
         else {
+            fila = document.createElement('tr');
+            for (let i = 0; i < form.elements.length; i++) {
+
+                if (form.elements[i].type != 'button') {
+
+                    contenido = form.elements[i].value;
+                    agregarCelda(fila, contenido);
+
+                }
+            }
+        }
+    }
+    else {
+        if (valid == false) {
+            boton=document.createElement('button');
+            boton.setAttribute('type','button');
+            boton.setAttribute('onclick','borrar()');
+            boton.innerHTML='Borrar';
+        form.appendChild(boton);
             
             fila = document.createElement('tr');
+
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Tarea')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
             tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Descripción')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Importancia')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Duración')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Realizado')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            celda = document.createElement('th');
+            celda.style.border = '1px solid';
+            contenido = document.createTextNode('Borrar')
+            celda.appendChild(contenido);
+            fila.appendChild(celda);
+            tabla.appendChild(fila);
+            form = document.getElementById('form');
+            fila = document.createElement('tr');
             for (let i = 0; i < form.elements.length; i++) {
-                if (form.elements[i].type !== 'button') {
-                    celda = document.createElement('td');
-                    celda.style.border = '1px solid';
-                    contenido = document.createTextNode(form.elements[i].value);
-                    celda.appendChild(contenido);
-                    fila.appendChild(celda);
+
+                if (form.elements[i].type != 'button') {
+
+                    contenido = form.elements[i].value;
+                    agregarCelda(fila, contenido);
 
                 }
             }
@@ -94,125 +144,71 @@ function almacenar() {
             celda.style.border = '1px solid';
             contenido = document.createElement('input');
             contenido.setAttribute('type', 'checkbox');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
-        }
-
-
-
-    }
-    else{
-       
-        if(valid==false){
-            document.body.innerHTML+='<button type="button" onclick="borrar()">Borrar</button>';
-            tabla = document.createElement('table');
-            document.body.appendChild(tabla);
-            tabla.style.border = '1px solid';
-           fila = document.createElement('tr');
-           tabla.appendChild(fila);
-           celda = document.createElement('th');
-           celda.style.border = '1px solid';
-           contenido = document.createTextNode('Tarea');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           celda = document.createElement('th');
-           celda.style.border = '1px solid';
-           contenido = document.createTextNode('Descripción');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           celda = document.createElement('th');
-           celda.style.border = '1px solid';
-           contenido = document.createTextNode('Importancia');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           celda = document.createElement('th');
-           celda.style.border = '1px solid';
-           contenido = document.createTextNode('Duración');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           celda = document.createElement('th');
-           celda.style.border = '1px solid';
-           contenido = document.createTextNode('Realizado');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           celda = document.createElement('th');
-           celda.style.border = '1px solid';
-           contenido = document.createTextNode('Borrar');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           fila = document.createElement('tr');
-           tabla.appendChild(fila);
-    
-           for (let i = 0; i < form.elements.length; i++) {
-               if (form.elements[i].type !== 'button') {
-                   celda = document.createElement('td');
-                   celda.style.border = '1px solid';
-                   contenido = document.createTextNode(form.elements[i].value);
-                   celda.appendChild(contenido);
-                   fila.appendChild(celda);
-    
-               }
-           }
-           celda = document.createElement('td');
-           celda.style.border = '1px solid';
-           contenido = document.createElement('input');
-           contenido.setAttribute('type', 'checkbox');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           celda = document.createElement('td');
-           celda.style.border = '1px solid';
-           contenido = document.createElement('input');
-           contenido.setAttribute('type', 'checkbox');
-           contenido.setAttribute('class','borrar');
-           celda.appendChild(contenido);
-           fila.appendChild(celda);
-           valid=true;
+            agregar(celda, contenido);
+            celda = document.createElement('td');
+            celda.style.border = '1px solid';
+            contenido = document.createElement('input');
+            contenido.setAttribute('type', 'checkbox');
+            contenido.setAttribute('class', 'borrar');
+           
+            agregar(celda, contenido);
+            valid = true;
         }
         else{
+            form = document.getElementById('form');
             fila = document.createElement('tr');
-            tabla.appendChild(fila);
-     
             for (let i = 0; i < form.elements.length; i++) {
-                if (form.elements[i].type !== 'button') {
-                    celda = document.createElement('td');
-                    celda.style.border = '1px solid';
-                    contenido = document.createTextNode(form.elements[i].value);
-                    celda.appendChild(contenido);
-                    fila.appendChild(celda);
-     
+
+                if (form.elements[i].type != 'button') {
+
+                    contenido = form.elements[i].value;
+                    agregarCelda(fila, contenido);
+
                 }
             }
             celda = document.createElement('td');
             celda.style.border = '1px solid';
             contenido = document.createElement('input');
             contenido.setAttribute('type', 'checkbox');
-            celda.appendChild(contenido);
-            fila.appendChild(celda);
+            agregar(celda, contenido);
             celda = document.createElement('td');
             celda.style.border = '1px solid';
             contenido = document.createElement('input');
             contenido.setAttribute('type', 'checkbox');
-            contenido.setAttribute('class','borrar');
-            celda.appendChild(contenido);
-            fila.appendChild(celda); 
+            contenido.setAttribute('class', 'borrar');
+           
+            agregar(celda, contenido);
         }
-      
     }
 
-
-
 }
-function borrar(){
 
-    let celdas=document.getElementsByClassName('borrar');
+
+function agregarCelda(fila, contenido) {
+    celda = document.createElement('td');
+    celda.style.border = '1px solid';
+    contenido = document.createTextNode(contenido);
+    celda.appendChild(contenido);
+    fila.appendChild(celda);
+    tabla.appendChild(fila);
+}
+
+function agregar(celda,contenido) {
+
+    celda.appendChild(contenido);
+    fila.appendChild(celda);
+    tabla.appendChild(fila);
+}
+
+function borrar(){
+    let borrar=document.getElementsByClassName('borrar');
     let padre;
-    for(let i=0;i<celdas.length;i++){
-        if(celdas[i].checked){
-            padre=celdas[i].parentNode;
+    for(let i=0;i<borrar.length;i++){
+        if(borrar[i].checked){
+            padre=borrar[i].parentNode;
             padre=padre.parentNode;
             padre.parentNode.removeChild(padre);
             i--;
         }
     }
-
 }
